2500b45e39b7ad0bd1f2540feb911d9fb7eee9ca
Sebastian Lundström <khueue@gmail.com>
2012-12-03 22:41:48 +0100

    Working room service
    - Listens to walk and look
    - 'action: {"command":"walk", "args":["username"]}' ->
      will move username into the next room, or the first one if
      username is either new or falls off the cliff (last room)
    - 'event: {"msg": "username awoke in room X"}' ->
      will be sent upon all moves
    - 'action: {"command":"look", "args":["username"]}' ->
      will reply with
      'event: {"msg": "username is in the same room as x, y, z..."}'

--- src/server.pl
+++ src/server.pl
@@ -18,7 +18,7 @@ connect_to_postal_service(Host, Port) :-
         close_connection(In, Out)).
 
 run_program(postal(PostalIn,PostalOut)) :-
-    register_for_actions(postal(PostalIn,PostalOut), [move]),
+    register_for_actions(postal(PostalIn,PostalOut), [walk,look]),
     setup_call_cleanup(
         (
             mongo:new_connection(Connection),
@@ -26,8 +26,7 @@ run_program(postal(PostalIn,PostalOut)) :-
             mongo:get_collection(Database, 'rooms', Collection)
         ),
         handle_service(PostalIn, PostalOut, Collection),
-        mongo:free_connection(Connection)),
-    write(xxxxxxxxxxx).
+        mongo:free_connection(Connection)).
 
 register_for_actions(postal(_PostalIn,PostalOut), Actions) :-
     json:doc_json(json([actions-Actions]), Payload),
@@ -43,44 +42,87 @@ handle_service(In, Out, Collection) :-
     handle_client_response(In, Out, Collection, Bytes).
 
 handle_client_response(In, Out, Collection, Bytes) :-
-    Bytes = [a,c,t,i,o,n,:,' '|BytesJson], % Does not handle 'action:' without trailing ws.
+    Bytes = [a,c,t,i,o,n,:,' '|BytesJson],
     !,
     core:atom_chars(JsonAtom, BytesJson),
     json:doc_json(Doc, JsonAtom),
     log('Parsed: ~w', [Doc]),
-    handle_action_walk(postal(In,Out), Collection, Doc),
+    json_get(Doc, command, Command),
+    handle_action(Command, postal(In,Out), Collection, Doc),
     handle_service(In, Out, Collection).
 handle_client_response(In, Out, Collection, Bytes) :-
     log('Unhandled: ~w', [Bytes]),
     handle_service(In, Out, Collection).
 
+handle_action(walk, postal(In,Out), Collection, Doc) :-
+    !,
+    handle_action_walk(postal(In,Out), Collection, Doc).
+handle_action(look, postal(In,Out), Collection, Doc) :-
+    !,
+    handle_action_look(postal(In,Out), Collection, Doc).
+handle_action(Command, _, _, _) :-
+    log('Unhandled command: ~w', [Command]).
+
 handle_action_walk(postal(In,Out), Collection, Doc) :-
     json_get(Doc, args, [User|_]),
     get_all_rooms(Collection, Rooms),
-    move_user(Rooms, User, num-Num, Rooms1),
+    move_user(Rooms, User, Out, num-Num, Rooms1),
     mongo:delete(Collection, []),
     mongo:insert(Collection, [rooms-Rooms1]),
-    core:atomic_list_concat(['event: {"msg":"',User,' entered room ', Num, '"}'], Msg),
+    core:atomic_list_concat(['event: {"msg":"',User,' awoke in room ',Num,'"}'], Msg),
     send_and_flush(Out, Msg).
 
-move_user(Rooms, User, num-N1, Rooms2) :-
-    remove_user_from_room(Rooms, User, num-N, Rooms1),
-    N1 is N + 1,
-    add_user_to_room(Rooms1, User, num-N1, Rooms2).
+handle_action_look(postal(In,Out), Collection, Doc) :-
+    json_get(Doc, args, [User|_]),
+    find_roommates(Collection, User, Roommates),
+    create_roommates_msg(Roommates, User, MsgAtom),
+    json:doc_json(json([msg-MsgAtom]), JsonAtom),
+    core:atomic_list_concat(['event: ',JsonAtom], Msg),
+    send_and_flush(Out, Msg).
 
-add_user_to_room([[num-N,users-Users]|Rooms], User, num-N, [[num-N,users-[User|Users]]|Rooms]) :-
+create_roommates_msg([], User, MsgAtom) :-
+    !,
+    core:atomic_list_concat([User,' is alone'], MsgAtom).
+create_roommates_msg(Roommates, User, MsgAtom) :-
+    core:atomic_list_concat(Roommates, ', ', RoommatesAtom),
+    core:atomic_list_concat([User,' is in the same room as ',RoommatesAtom], MsgAtom).
+
+find_roommates(Collection, User, Roommates) :-
+    get_all_rooms(Collection, Rooms),
+    search_rooms(Rooms, User, Roommates).
+
+search_rooms([], _, []).
+search_rooms([[Num,users-Users]|Rooms], User, Roommates) :-
+    delete(Users, User, Roommates),
     !.
-add_user_to_room([Room|Rooms], User, Num, [Room|Rooms1]) :-
-    add_user_to_room(Rooms, User, Num, Rooms1).
+search_rooms([Room|Rooms], User, Roommates) :-
+    search_rooms(Rooms, User, Roommates).
 
-remove_user_from_room([[num-N,users-Users]], User, num-0, [[num-N,users-Users1]]) :-
+move_user(Rooms, User, Out, num-N1, Rooms2) :-
+    remove_user_from_room(Rooms, User, Out, num-N, Rooms1),
+    N1 is N + 1,
+    add_user_to_room(Rooms1, User, Out, num-N1, Rooms2).
+
+add_user_to_room([[num-N,users-Users]|Rooms], User, Out, num-N, [[num-N,users-[User|Users]]|Rooms]) :-
+    !.
+add_user_to_room([Room|Rooms], User, Out, Num, [Room|Rooms1]) :-
+    add_user_to_room(Rooms, User, Out, Num, Rooms1).
+
+remove_user_from_room([[num-N,users-Users]], User, Out, num-0, [[num-N,users-Users1]]) :-
+    delete(Users, User, Users1),
+    !,
+    core:atomic_list_concat(['event: {"msg":"',User,' fell off a cliff"}'], Msg),
+    send_and_flush(Out, Msg),
+    core:atomic_list_concat(['event: {"msg":"',User,' was reincarnated in room 1"}'], Msg2),
+    send_and_flush(Out, Msg2).
+remove_user_from_room([[num-N,users-Users]], User, Out, num-0, [[num-N,users-Users1]]) :-
     !,
     try_delete(Users, User, Users1).
-remove_user_from_room([[num-N,users-Users]|Rooms], User, num-N, [[num-N,users-Users1]|Rooms]) :-
+remove_user_from_room([[num-N,users-Users]|Rooms], User, Out, num-N, [[num-N,users-Users1]|Rooms]) :-
     delete(Users, User, Users1),
     !.
-remove_user_from_room([Room|Rooms], User, Num, [Room|Rooms1]) :-
-    remove_user_from_room(Rooms, User, Num, Rooms1).
+remove_user_from_room([Room|Rooms], User, Out, Num, [Room|Rooms1]) :-
+    remove_user_from_room(Rooms, User, Out, Num, Rooms1).
 
 try_delete([], _, []) :- !.
 try_delete([X|Xs], X, Xs) :- !.
@@ -114,7 +156,8 @@ create_n_rooms(N, [[num-N,users-[]]|Rooms]) :-
 
 send_and_flush(Socket, Message) :-
     core:format(Socket, '~w~n', [Message]),
-    core:flush_output(Socket).
+    core:flush_output(Socket),
+    sleep(0.1). % Just to please Akka ...
 
 log(Format, Args) :-
     core:atomic_list_concat([Format,'~n'], Format1),
@@ -123,8 +166,6 @@ log(Format, Args) :-
 
 receive_line(In, Bytes) :-
     core:get_char(In, Byte),
-    %format('~w', [Byte]),
-    %flush_output,
     receive_line_aux(In, Byte, Bytes).
 
 receive_line_aux(In, Byte, []) :-
@@ -133,19 +174,8 @@ receive_line_aux(In, Byte, []) :-
 receive_line_aux(In, Byte, [Byte|Bytes]) :-
     receive_line(In, Bytes).
 
-remove_end_of_line_crap(In, Byte) :-
-    ( stop_char(Byte) ; whitespace(Byte) ),
-    !,
-    core:get_char(In, _),
-    core:peek_byte(In, Byte2),
-    remove_end_of_line_crap(In, Byte2).
-remove_end_of_line_crap(_, _).
-
 stop_char('\n').
 stop_char('\r').
-
-whitespace(' ').
-whitespace('\t').
 
 json_get(json(Pairs), Key, Value) :-
     json_get_aux(Pairs, Key, Value).
@@ -153,50 +183,3 @@ json_get(json(Pairs), Key, Value) :-
 json_get_aux([Key-Value|_], Key, Value) :- !.
 json_get_aux([_Pair|Pairs], Key, Value) :-
     json_get_aux(Pairs, Key, Value).
-
-% register: {"actions":["move"]}
-% action: {"command":"move", "args":["seb","left"]}
-
-% göra register: move till akka
-% lyssna på action command move
-% skicka event: {message: "marten gick till aoeu"}
-
-/*
-[room(1, ["seb","akka"]), room(2, ["mårten"])]
-*/
-
-
-
-
-
-
-
-
-
-/*
-
-create_server(Port) :-
-    tcp_socket(Socket),
-    tcp_setopt(Socket, reuseaddr),
-    tcp_bind(Socket, Port),
-    tcp_listen(Socket, 5),
-    tcp_open_socket(Socket, AcceptFd, _),
-    dispatch(AcceptFd).
-
-dispatch(AcceptFd) :-
-    tcp_accept(AcceptFd, Socket, Peer),
-    thread_create(
-        process_client(Socket, Peer),
-        _,
-        [detached(true)]),
-    dispatch(AcceptFd).
-
-process_client(Socket, _Peer) :-
-    setup_call_cleanup(
-        tcp_open_socket(Socket, In, Out),
-        setup_call_cleanup(
-            mongo:new_connection(Collection),
-            handle_service(In, Out, Collection),
-            mongo:free_connection(Collection)),
-        close_connection(In, Out)).
-*/
